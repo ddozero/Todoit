e0817a5bf586b5f9cf0c9c5c9549af06f0c8f90f
doyeong <dozero@naver.com>
2026-03-21 15:02:58 +0900

    Feat: Spring Boot 3 & Java 17 Project Setup

---
+++ src/main/java/com/todoit/TodoitApplication.java
@@ -0,0 +1,13 @@
+package com.todoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoit/TodoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
