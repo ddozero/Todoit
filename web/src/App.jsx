96d01403fd326d6661448a34747ed26dfb2d6b70
doyeong <kdoyeong1@gmail.com>
2026-04-05 17:06:32 +0900

    ✨ Feat: add common header and footer

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,25 +1,41 @@
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Header from "./components/common/Header.jsx";
+
 import Login from "./components/intro/Login.jsx";
 import Signup from "./components/intro/Signup.jsx";
 import Profile from "./components/mypage/Profile.jsx";
+import './App.css';
+
+function Layout({ children }) {
+  const location = useLocation();
+  const showCommon = location.pathname !== "/";
+
+  return (
+    <>
+      {showCommon && <Header />}
+      <main>{children}</main>
+    </>
+  );
+}
 
 function App() {
 
-    return (
-      <Router>
+  return (
+    <Router>
+      <Layout>
         <Routes>
-            {/*1.기본경로페이지 */}
-            <Route path="/" element={<Login />}/>
+          {/*1.기본경로페이지 */}
+          <Route path="/" element={<Login />} />
 
-            {/*2.회원가입페이지*/}
-            <Route path="/signup" element={<Signup />}/>
+          {/*2.회원가입페이지*/}
+          <Route path="/signup" element={<Signup />} />
 
-            {/*3.프로필페이지*/}
-            <Route path="/mypage" element={<Profile />}/>
-
+          {/*3.프로필페이지*/}
+          <Route path="/mypage" element={<Profile />} />
         </Routes>
-      </Router>
-    )
+      </Layout>
+    </Router>
+  )
 }
 export default App;
---
+++ web/src/components/common/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return(
+        <header className="main-header">
+           <div className="header-container">
+                <h1 className="logo-title">TODOIT</h1>
+                <p className="logo-subtitle">나도 취준 할 수 있다!</p>
+           </div>
+        </header>
+    )
+}
+
+export default Header;
